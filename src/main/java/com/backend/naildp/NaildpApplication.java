ab637d27c3c8ec77b25c5cb3a67194265be89540
minji-kim525 <minjik0525@gmail.com>
2024-06-17 11:32:45 +0900

    [CONF] init project

---
+++ src/main/java/com/backend/naildp/NaildpApplication.java
@@ -0,0 +1,13 @@
+package com.backend.naildp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaildpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaildpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/naildp/NaildpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.naildp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaildpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
