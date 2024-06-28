e9c7c37a4456b22e8fe0a05a67d5b1f566c036e4
minji-kim525 <minjik0525@gmail.com>
2024-06-28 12:04:28 +0900

    fix : 카카오 로그인 uri mismatch 오류 해결

--- src/main/java/com/backend/naildp/controller/AuthController.java
+++ src/main/java/com/backend/naildp/controller/AuthController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +20,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api")
 @RequiredArgsConstructor
 public class AuthController {
 
@@ -42,7 +40,7 @@ public class AuthController {
 	// 	return authService.signupUser(loginRequestDto);
 	// }
 
-	@GetMapping("/auth/kakao/callback")
+	@GetMapping("/auth/kakao")
 	public ResponseEntity<?> kakaoLogin(@RequestParam("code") String code, HttpServletResponse response) throws
 		JsonProcessingException {
 
