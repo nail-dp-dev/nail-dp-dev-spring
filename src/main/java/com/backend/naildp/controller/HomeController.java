1c30b9e1420b760fa38018fcdff813cc56ded3d4
ostar11 <cjw5877@gmail.com>
2024-06-28 14:44:45 +0900

    feat : 공통 응답 포맷 적용 #7

--- src/main/java/com/backend/naildp/controller/HomeController.java
+++ src/main/java/com/backend/naildp/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.backend.naildp.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.backend.naildp.common.ApiResponse;
+import com.backend.naildp.dto.home.HomePostResponse;
 import com.backend.naildp.service.PostService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,6 +26,7 @@ public class HomeController {
 	@GetMapping
 	public ResponseEntity<?> homePosts(@RequestParam(name = "choice") String choice, @AuthenticationPrincipal
 		UserDetails userDetails) {
-		return ResponseEntity.ok(postService.homePosts(userDetails.getUsername()));
+		List<HomePostResponse> homePostResponses = postService.homePosts(userDetails.getUsername());
+		return ResponseEntity.ok(ApiResponse.successResponse(homePostResponses));
 	}
 }
