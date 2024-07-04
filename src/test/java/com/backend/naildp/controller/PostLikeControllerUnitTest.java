6214cbb35a045520ffe945f3744c3e394d155896
ostar11 <cjw5877@gmail.com>
2024-07-04 13:54:55 +0900

    test : 게시물 좋아요 API 예외 테스트 작성 #13
    PostLikeService.likeByPostId()의 게시물 조회 로직에서 예외 발생할 때 응답 확인 테스트 작성

--- src/test/java/com/backend/naildp/controller/PostLikeControllerUnitTest.java
+++ src/test/java/com/backend/naildp/controller/PostLikeControllerUnitTest.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +17,8 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.backend.naildp.exception.ApiResponse;
+import com.backend.naildp.exception.CustomException;
+import com.backend.naildp.exception.ErrorCode;
 import com.backend.naildp.service.PostLikeService;
 
 @WebMvcTest(PostLikeController.class)
@@ -41,6 +44,26 @@ class PostLikeControllerUnitTest {
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
 			.andExpect(jsonPath("$.message").value(apiResponse.getMessage()))
 			.andExpect(jsonPath("$.code").value(apiResponse.getCode()));
+	}
+
+	@DisplayName("게시물 좋아요 API 예외 테스트 - 게시물 조회 예외")
+	@Test
+	@WithMockUser(username = "testUser", roles = {"USER"})
+	void likePostApiExceptionByPost() throws Exception {
+		Long resultId = 10L;
+
+		ApiResponse<?> apiResponse = ApiResponse.of(ErrorCode.NOT_FOUND);
+		apiResponse.setMessage("해당 포스트를 조회할 수 없습니다.");
+
+		when(postLikeService.likeByPostId(anyLong(), anyString()))
+			.thenThrow(new CustomException("해당 포스트를 조회할 수 없습니다.", ErrorCode.NOT_FOUND));
+
+		mvc.perform(post("/posts/{postId}/likes", 3L).with(csrf()))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.message").value(apiResponse.getMessage()))
+			.andExpect(jsonPath("$.code").value(apiResponse.getCode()))
+			.andDo(print());
 	}
 
 	@DisplayName("게시물 좋아요 취소 API 테스트")
