931cbf62cc2521c9324f69ccd9a16ce432d6bb86
ostar11 <cjw5877@gmail.com>
2024-07-04 11:59:03 +0900

    test : PostLikeController 게시물 좋아요 API 단위 테스트 작성 #13

---
+++ src/test/java/com/backend/naildp/controller/PostLikeControllerUnitTest.java
@@ -0,0 +1,46 @@
+package com.backend.naildp.controller;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.backend.naildp.exception.ApiResponse;
+import com.backend.naildp.service.PostLikeService;
+
+@WebMvcTest(PostLikeController.class)
+class PostLikeControllerUnitTest {
+
+	@Autowired
+	MockMvc mvc;
+
+	@MockBean
+	PostLikeService postLikeService;
+
+	@DisplayName("게시물 좋아요 API 테스트")
+	@Test
+	@WithMockUser(username = "testUser", roles = {"USER"})
+	void likePostApiTest() throws Exception {
+		Long resultId = 10L;
+		ApiResponse<Object> apiResponse = ApiResponse.successResponse(null, "좋아요 목록에 추가", 2001);
+
+		when(postLikeService.likeByPostId(anyLong(), anyString())).thenReturn(resultId);
+
+		mvc.perform(post("/posts/{postId}/likes", 3L).with(csrf()))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+			.andExpect(jsonPath("$.message").value(apiResponse.getMessage()))
+			.andExpect(jsonPath("$.code").value(apiResponse.getCode()));
+	}
+
+}
