3604249058fddc6f42f8ecc83c23a4a02fb6c6da
ostar11 <cjw5877@gmail.com>
2024-07-05 11:30:29 +0900

    test : 게시물 좋아요 취소 예외 테스트 작성 #13
    사용자가 해당 게시글을 좋아요하지 않은 상태면 예외 발생

--- src/test/java/com/backend/naildp/service/PostLikeServiceUnitTest.java
+++ src/test/java/com/backend/naildp/service/PostLikeServiceUnitTest.java
@@ -4,6 +4,7 @@ import static org.mockito.BDDMockito.*;
 
 import java.util.Optional;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,6 +18,8 @@ import com.backend.naildp.dto.LoginRequestDto;
 import com.backend.naildp.entity.Post;
 import com.backend.naildp.entity.PostLike;
 import com.backend.naildp.entity.User;
+import com.backend.naildp.exception.CustomException;
+import com.backend.naildp.exception.ErrorCode;
 import com.backend.naildp.repository.PostLikeRepository;
 import com.backend.naildp.repository.PostRepository;
 import com.backend.naildp.repository.UserRepository;
@@ -80,5 +83,25 @@ class PostLikeServiceUnitTest {
 		//then
 		verify(postLikeRepository).findPostLikeByUserNicknameAndPostId(anyString(), anyLong());
 		verify(postLikeRepository).deletePostLikeById(postLike.getId());
+	}
+
+	@DisplayName("게시물 좋아요 취소 예외 테스트")
+	@Test
+	void cancelPostLikeException() {
+		//given
+		Long postId = 1L;
+		String nickname = "nickname";
+		String wrongNickname = "wrong";
+		LoginRequestDto loginRequestDto = new LoginRequestDto(nickname, "phoneNumber", true);
+		User user = new User(loginRequestDto, UserRole.USER);
+		Post post = new Post(user, "content", 0L, Boundary.ALL, false);
+		PostLike postLike = new PostLike(user, post);
+
+		given(postLikeRepository.findPostLikeByUserNicknameAndPostId(anyString(), anyLong()))
+			.willThrow(new CustomException("해당 게시물은 존재하지 않습니다.", ErrorCode.NOT_FOUND));
+
+		//then
+		Assertions.assertThatThrownBy(() -> postLikeService.unlikeByPostId(postId, nickname))
+			.isInstanceOf(CustomException.class);
 	}
 }
