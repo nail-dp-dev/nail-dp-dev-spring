672db317242c9dbf341c1781427005b2de7a98c3
ostar11 <cjw5877@gmail.com>
2024-07-10 17:24:50 +0900

    test : PostService 좋아요한 게시글 조회 단위 테스트 작성 #21

--- src/test/java/com/backend/naildp/service/PostServiceUnitTest.java
+++ src/test/java/com/backend/naildp/service/PostServiceUnitTest.java
@@ -1,6 +1,7 @@
 package com.backend.naildp.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.auditing.AuditingHandler;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -20,6 +21,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import com.backend.naildp.common.Boundary;
+import com.backend.naildp.config.JpaAuditingConfiguration;
 import com.backend.naildp.dto.home.HomePostResponse;
 import com.backend.naildp.entity.ArchivePost;
 import com.backend.naildp.entity.Photo;
@@ -30,6 +32,7 @@ import com.backend.naildp.repository.PostLikeRepository;
 import com.backend.naildp.repository.PostRepository;
 
 @ExtendWith(MockitoExtension.class)
+@Import(JpaAuditingConfiguration.class)
 class PostServiceUnitTest {
 
 	@InjectMocks
@@ -67,9 +70,9 @@ class PostServiceUnitTest {
 			.map(post -> new PostLike(null, post))
 			.collect(Collectors.toList());
 
-		Mockito.when(postRepository.findPostsAndPhotoByBoundary(Boundary.ALL, pageRequest)).thenReturn(pagedPost);
-		Mockito.when(archivePostRepository.findAllByArchiveUserNickname(nickname)).thenReturn(archivePosts);
-		Mockito.when(postLikeRepository.findAllByUserNickname(nickname)).thenReturn(postLikes);
+		when(postRepository.findPostsAndPhotoByBoundary(Boundary.ALL, pageRequest)).thenReturn(pagedPost);
+		when(archivePostRepository.findAllByArchiveUserNickname(nickname)).thenReturn(archivePosts);
+		when(postLikeRepository.findAllByUserNickname(nickname)).thenReturn(postLikes);
 
 		//when
 		List<HomePostResponse> homePostResponses = postService.homePosts(nickname);
@@ -105,6 +108,39 @@ class PostServiceUnitTest {
 		// }
 	}
 
+	@DisplayName("좋아요한 게시글 조회 테스트")
+	@Test
+	void findPagedLikedPost() {
+		//given
+		String nickname = "mj";
+		int postCnt = 20;
+		int pageSize = 20;
+		PageRequest pageRequest = PageRequest.of(0, pageSize, Sort.by(Sort.Direction.DESC, "createdDate"));
+
+		List<PostLike> postLikes = createPostLikes(postCnt);
+		PageImpl<PostLike> pagedPostLikes = new PageImpl<>(postLikes, pageRequest, pageSize);
+		Page<Post> pagedPosts = pagedPostLikes.map(PostLike::getPost);
+		List<ArchivePost> archivePosts = pagedPosts.stream()
+			.map(post -> new ArchivePost(null, post))
+			.collect(Collectors.toList());
+
+		when(postLikeRepository.findPagedPostLikesByBoundaryOpened(any(PageRequest.class), anyString(),
+			any(Boundary.class))).thenReturn(pagedPostLikes);
+		when(archivePostRepository.findAllByArchiveUserNickname(nickname)).thenReturn(archivePosts);
+
+		//when
+		Page<HomePostResponse> likedPostResponses = postService.findLikedPost(nickname, 0);
+		Page<Boolean> savedList = likedPostResponses.map(HomePostResponse::getSaved);
+		Page<Boolean> likedList = likedPostResponses.map(HomePostResponse::getLike);
+
+		//then
+		verify(postLikeRepository).findPagedPostLikesByBoundaryOpened(any(PageRequest.class), anyString(),
+			any(Boundary.class));
+		verify(archivePostRepository).findAllByArchiveUserNickname(nickname);
+		assertThat(savedList.getTotalElements()).isEqualTo(20);
+		assertThat(likedList.getTotalElements()).isEqualTo(20);
+	}
+
 	private List<Post> createTestPosts(int postCnt) {
 		List<Post> posts = new ArrayList<>();
 		for (int i = 0; i < postCnt; i++) {
@@ -117,4 +153,13 @@ class PostServiceUnitTest {
 		return posts;
 	}
 
+	private List<PostLike> createPostLikes(int postCnt) {
+		List<PostLike> postLikes = new ArrayList<>();
+		List<Post> posts = createTestPosts(postCnt);
+		posts.forEach(post -> {
+			PostLike postLike = new PostLike(null, post);
+			postLikes.add(postLike);
+		});
+		return postLikes;
+	}
 }
