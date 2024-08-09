ea37916362bb0fe825416fc42e7181b8afe70f73
ostar11 <cjw5877@gmail.com>
2024-08-09 12:01:48 +0900

    feat : 게시물 공개범위 설정 #52

---
+++ src/main/java/com/backend/naildp/dto/post/PostBoundaryRequest.java
@@ -0,0 +1,15 @@
+package com.backend.naildp.dto.post;
+
+import com.backend.naildp.common.Boundary;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostBoundaryRequest {
+
+	private Boundary closer;
+}
