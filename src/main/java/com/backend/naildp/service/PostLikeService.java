665b6cde29d83343caf3245d9d05164d463f84f1
ostar11 <cjw5877@gmail.com>
2024-08-10 17:24:17 +0900

    feat : 댓글 좋아요 수 조회 API 구현 #54

--- src/main/java/com/backend/naildp/service/PostLikeService.java
+++ src/main/java/com/backend/naildp/service/PostLikeService.java
@@ -3,11 +3,13 @@ package com.backend.naildp.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.backend.naildp.dto.postLike.PostLikeCountResponse;
 import com.backend.naildp.entity.Post;
 import com.backend.naildp.entity.PostLike;
 import com.backend.naildp.entity.User;
 import com.backend.naildp.exception.CustomException;
 import com.backend.naildp.exception.ErrorCode;
+import com.backend.naildp.repository.FollowRepository;
 import com.backend.naildp.repository.PostLikeRepository;
 import com.backend.naildp.repository.PostRepository;
 import com.backend.naildp.repository.UserRepository;
@@ -22,6 +24,7 @@ public class PostLikeService {
 	private final UserRepository userRepository;
 	private final PostRepository postRepository;
 	private final PostLikeRepository postLikeRepository;
+	private final FollowRepository followRepository;
 
 	@Transactional
 	public Long likeByPostId(Long postId, String username) {
@@ -43,5 +46,29 @@ public class PostLikeService {
 			.orElseThrow(() -> new CustomException("해당 게시물은 존재하지 않습니다.", ErrorCode.NOT_FOUND));
 
 		postLikeRepository.deletePostLikeById(postLike.getId());
+	}
+
+
+	public PostLikeCountResponse countPostLike(Long postId, String username) {
+		Post post = postRepository.findPostAndUser(postId)
+			.orElseThrow(() -> new CustomException("게시물을 열람할 수 없습니다.", ErrorCode.USER_MISMATCH));
+		User postWriter = post.getUser();
+
+		//post 에 댓글 달 수 있는지 확인
+		//1. tempSave = false 인지
+		//2. boundary 가 All 이거나 boundary 가 Follow 면서 username 이 팔로우 하고 있는지까지 확인 필요
+		if (post.isTempSaved()) {
+			throw new CustomException("임시저장한 게시물은 좋아요를 볼 수 없습니다.", ErrorCode.NOT_FOUND);
+		}
+
+		if (post.isClosed() && !postWriter.equalsNickname(username)) {
+			throw new CustomException("비공개 게시물은 좋아요를 볼 수 없습니다.", ErrorCode.INVALID_BOUNDARY);
+		}
+
+		if (post.isOpenedForFollower() && !followRepository.existsByFollowerNicknameAndFollowing(username, postWriter)) {
+			throw new CustomException("팔로워 공개 게시물입니다. 팔로워만 좋아요할 수 있습니다.", ErrorCode.INVALID_BOUNDARY);
+		}
+
+		return new PostLikeCountResponse(post.getPostLikes().size());
 	}
 }
