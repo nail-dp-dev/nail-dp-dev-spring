876e1168d244f0daef273b6de9c2684ae8d6741b
ostar11 <cjw5877@gmail.com>
2024-08-12 15:03:00 +0900

    refactor : 댓글 좋아요 로직에서 댓글 찾기 수정 #55

--- src/main/java/com/backend/naildp/service/CommentLikeService.java
+++ src/main/java/com/backend/naildp/service/CommentLikeService.java
@@ -13,6 +13,7 @@ import com.backend.naildp.entity.User;
 import com.backend.naildp.exception.CustomException;
 import com.backend.naildp.exception.ErrorCode;
 import com.backend.naildp.repository.CommentLikeRepository;
+import com.backend.naildp.repository.CommentRepository;
 import com.backend.naildp.repository.FollowRepository;
 import com.backend.naildp.repository.PostRepository;
 import com.backend.naildp.repository.UserRepository;
@@ -27,6 +28,7 @@ public class CommentLikeService {
 	private final PostRepository postRepository;
 	private final FollowRepository followRepository;
 	private final UserRepository userRepository;
+	private final CommentRepository commentRepository;
 	private final CommentLikeRepository commentLikeRepository;
 
 	@Transactional
@@ -35,28 +37,20 @@ public class CommentLikeService {
 			.orElseThrow(() -> new CustomException("게시물을 찾을 수 없습니다.", ErrorCode.NOT_FOUND));
 		User postWriter = post.getUser();
 
-		//post tempSave 확인
 		if (post.isTempSaved()) {
 			throw new CustomException("임시저장한 게시물에는 댓글을 등록할 수 없습니다.", ErrorCode.NOT_FOUND);
 		}
 
-		//post boundary 확인
-		//  all 이면 모든 사람
-		//  follow 면서 follower 나 작성자가 아니면 예외 -> 좋아요 안됨
-		//  none 이면 작성자만 가능
 		if (post.isClosed() && post.notWrittenBy(username)) {
 			throw new CustomException("비공개 게시물은 작성자만 접근할 수 있습니다.", ErrorCode.INVALID_BOUNDARY);
 		}
 
-		if (post.isOpenedForFollower() && (followRepository.existsByFollowerNicknameAndFollowing(username, postWriter)
-			|| post.notWrittenBy(username))) {
+		if (post.isOpenedForFollower() && followRepository.existsByFollowerNicknameAndFollowing(username, postWriter)
+			&& post.notWrittenBy(username)) {
 			throw new CustomException("팔로우 공개 게시물은 팔로워와 작성자만 접근할 수 있습니다.", ErrorCode.INVALID_BOUNDARY);
 		}
 
-		Comment findComment = post.getComments()
-			.stream()
-			.filter(comment -> Objects.equals(comment.getId(), commentId))
-			.findFirst()
+		Comment findComment = commentRepository.findById(commentId)
 			.orElseThrow(() -> new CustomException("댓글을 찾을 수 없습니다.", ErrorCode.NOT_FOUND));
 
 		User user = userRepository.findByNickname(username)
