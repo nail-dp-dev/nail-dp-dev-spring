ae20cd682e59c2c42f927da759f32cb862a0b753
ostar11 <cjw5877@gmail.com>
2024-08-13 17:10:16 +0900

    test : CommentServiceTest 리펙토링 #43

--- src/test/java/com/backend/naildp/service/CommentServiceTest.java
+++ src/test/java/com/backend/naildp/service/CommentServiceTest.java
@@ -1,9 +1,11 @@
 package com.backend.naildp.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -35,42 +37,25 @@ class CommentServiceTest {
 
 	@BeforeEach
 	void setup() {
-		User postWriter = User.builder().nickname("postWriter").phoneNumber("pn").agreement(true).role(UserRole.USER).build();
-		em.persist(postWriter);
-		User commenter = User.builder().nickname("commenter").phoneNumber("pn").agreement(true).role(UserRole.USER).build();
-		em.persist(commenter);
-		User noCommentUser = User.builder()
-			.nickname("noCommentUser")
-			.phoneNumber("pn")
-			.agreement(true)
-			.role(UserRole.USER)
-			.build();
-		em.persist(noCommentUser);
+		User postWriter = createUser("postWriter");
+		User commenter = createUser("commenter");
+		User noCommentUser = createUser("noCommentUser");
 
-		Post post = Post.builder().user(postWriter).postContent("content").tempSave(false).boundary(Boundary.ALL).build();
-		em.persist(post);
-		Post noCommentPost = Post.builder()
-			.user(noCommentUser)
-			.postContent("noComment")
-			.tempSave(false)
-			.boundary(Boundary.ALL)
-			.build();
-		em.persist(noCommentPost);
+		Post post = createPost(postWriter, "content");
+		Post noCommentPost = createPost(noCommentUser, "noComment");
 
 		for (int i = 0; i < COMMENT_CNT; i++) {
-			Comment comment = new Comment(commenter, post, "comment");
-			em.persist(comment);
+			Comment comment = createComment(commenter, post);
 		}
 	}
 
+	@DisplayName("댓글 조회 테스트 - 첫번째 페이지")
 	@Test
-	void 댓글_첫번째_페이지_조회_테스트() {
+	void readCommentsInFirstPage() {
 		//given
 		String userNickname = "postWriter";
 		int pageSize = COMMENT_CNT - 1;
-		Post post = em.createQuery("select p from Post p where p.user.nickname = :nickname", Post.class)
-			.setParameter("nickname", "postWriter")
-			.getSingleResult();
+		Post post = findPostByWriterNickname("postWriter");
 
 		//when
 		CommentSummaryResponse response = commentService.findComments(post.getId(), pageSize, FIRST_CURSOR_ID, userNickname);
@@ -80,17 +65,16 @@ class CommentServiceTest {
 		assertNotEquals(-1L, response.getCursorId());
 		assertTrue(contents.hasNext());
 		assertEquals(pageSize, contents.getNumberOfElements());
-		Assertions.assertThat(contents).extracting(CommentInfoResponse::getProfileUrl).containsOnly("default");
+		assertThat(contents).extracting(CommentInfoResponse::getProfileUrl).containsOnly("default");
 	}
 
+	@DisplayName("댓글 조회 테스트 - 댓글수보다 페이지가 클때")
 	@Test
-	void 댓글_조회_테스트_댓글수보다_페이지가_클때() {
+	void readCommentsAbovePageElements() {
 		//given
 		String userNickname = "postWriter";
 		int pageSize = COMMENT_CNT + 1;
-		Post post = em.createQuery("select p from Post p where p.user.nickname = :nickname", Post.class)
-			.setParameter("nickname", "postWriter")
-			.getSingleResult();
+		Post post = findPostByWriterNickname("postWriter");
 
 		//when
 		CommentSummaryResponse response = commentService.findComments(post.getId(), pageSize, FIRST_CURSOR_ID, userNickname);
@@ -100,16 +84,15 @@ class CommentServiceTest {
 		assertNotEquals(-1L, response.getCursorId());
 		assertFalse(contents.hasNext());
 		assertNotEquals(pageSize, contents.getNumberOfElements());
-		Assertions.assertThat(contents).extracting(CommentInfoResponse::getProfileUrl).containsOnly("default");
+		assertThat(contents).extracting(CommentInfoResponse::getProfileUrl).containsOnly("default");
 	}
 
+	@DisplayName("댓글 조회 테스트 - 댓글이 없을때")
 	@Test
-	void 댓글_조회_테스트_댓글이_없을때() {
+	void readNoComments() {
 		String userNickname = "postWriter";
 		int pageSize = 20;
-		Post post = em.createQuery("select p from Post p where p.user.nickname = :nickname", Post.class)
-			.setParameter("nickname", "noCommentUser")
-			.getSingleResult();
+		Post post = findPostByWriterNickname("noCommentUser");
 
 		//when
 		CommentSummaryResponse response = commentService.findComments(post.getId(), pageSize, FIRST_CURSOR_ID, userNickname);
@@ -121,4 +104,32 @@ class CommentServiceTest {
 		assertEquals(0, contents.getNumberOfElements());
 	}
 
+	private User createUser(String postWriter) {
+		User user = User.builder().nickname(postWriter).phoneNumber("pn").agreement(true).role(UserRole.USER).build();
+		em.persist(user);
+		return user;
+	}
+
+	private Post createPost(User postWriter, String content) {
+		Post post = Post.builder()
+			.user(postWriter)
+			.postContent(content)
+			.tempSave(false)
+			.boundary(Boundary.ALL)
+			.build();
+		em.persist(post);
+		return post;
+	}
+
+	private Comment createComment(User commenter, Post post) {
+		Comment comment = new Comment(commenter, post, "comment");
+		em.persist(comment);
+		return comment;
+	}
+
+	private Post findPostByWriterNickname(String postWriter) {
+		return em.createQuery("select p from Post p where p.user.nickname = :nickname", Post.class)
+			.setParameter("nickname", postWriter)
+			.getSingleResult();
+	}
 }
