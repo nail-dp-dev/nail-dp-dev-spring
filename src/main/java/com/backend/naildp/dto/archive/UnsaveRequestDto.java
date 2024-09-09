f6bc9064922e7212bda231691d9ecfc248c70aef
minji-kim525 <105044626+minji-kim525@users.noreply.github.com>
2024-09-09 10:38:52 +0900

    validate bug 수정

--- src/main/java/com/backend/naildp/dto/archive/UnsaveRequestDto.java
+++ src/main/java/com/backend/naildp/dto/archive/UnsaveRequestDto.java
@@ -3,6 +3,7 @@ package com.backend.naildp.dto.archive;
 import java.util.List;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UnsaveRequestDto {
-	@NotEmpty(message = "postId 값이 없습니다")
+	@NotNull(message = "postId 값이 없습니다")
 	private Long postId;
 	@NotEmpty(message = "archiveId 값이 없습니다")
 	private List<Long> archiveId;
