e719fbeaf50dc4edf52fd0d09513a0e5d8fbf4eb
ostar11 <cjw5877@gmail.com>
2024-10-17 03:39:51 +0900

    feat : 팔로우 알림 생성 로직에 TransactionalEventListener 적용 #116

--- src/main/java/com/backend/naildp/service/NotificationService.java
+++ src/main/java/com/backend/naildp/service/NotificationService.java
@@ -3,9 +3,13 @@ package com.backend.naildp.service;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.backend.naildp.common.NotificationType;
+import com.backend.naildp.entity.Follow;
 import com.backend.naildp.entity.Notification;
 import com.backend.naildp.entity.Post;
 import com.backend.naildp.entity.User;
@@ -15,7 +19,9 @@ import com.backend.naildp.repository.NotificationRepository;
 import com.backend.naildp.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -25,19 +31,20 @@ public class NotificationService {
 	private final UserRepository userRepository;
 	private final SseService sseService;
 
-	@Transactional
-	public long generateFollowNotification(UUID publisherId, UUID subscriberId) {
-		// 알림 생성 및 저장
-		User receiver = userRepository.findById(subscriberId)
-			.orElseThrow(() -> new CustomException("사용자를 찾을 수 없습니다.", ErrorCode.NOT_FOUND));
-		User followerUser = userRepository.findById(publisherId)
-			.orElseThrow(() -> new CustomException("사용자를 찾을 수 없습니다.", ErrorCode.NOT_FOUND));
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public long generateFollowNotification(Follow follow) {
+		log.info("follow transactionalEventListener -> 새로운 transaction 시작");
+		User followerUser = follow.getFollower();
+		User followingUser = follow.getFollowing();
+
 		Notification followNotification = Notification.builder()
-			.receiver(receiver)
-			.content(followerUser.getNickname() + "가 회원님을 팔로우했습니다.")
+			.receiver(followingUser)
+			.content(followerUser.getNickname() + "님이 회원님을 팔로우했습니다.")
 			.notificationType(NotificationType.FOLLOW)
 			.isRead(false)
 			.build();
+
 		Notification savedNotification = notificationRepository.save(followNotification);
 
 		//팔로우 푸시 알림 전송
