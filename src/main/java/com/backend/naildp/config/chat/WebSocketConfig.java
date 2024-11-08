a862cf5b507aa89b934982c0672e506a8d306997
minji-kim525 <105044626+minji-kim525@users.noreply.github.com>
2024-11-08 18:47:04 +0900

    fix: ws-stomp connection 문제

--- src/main/java/com/backend/naildp/config/chat/WebSocketConfig.java
+++ src/main/java/com/backend/naildp/config/chat/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/api/ws-stomp")
+		registry.addEndpoint("/ws-stomp")
 			.setAllowedOriginPatterns("*")
 			.addInterceptors(handshakeInterceptor)
 			.withSockJS()
